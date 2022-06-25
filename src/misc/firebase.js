a12c613c0f0c12b847bc218862e9ad97e3158f5b
Luxx <lakshrajme@gmail.com>
2022-06-25 14:35:20 +0530

    init Firebase in App

---
+++ src/misc/firebase.js
@@ -0,0 +1,8 @@
+const Config = {
+  apiKey: 'AIzaSyC5vVAOo7ZyiZ3BLcUQsd0yq2oSXTI8dmw',
+  authDomain: 'chatapp-luxx.firebaseapp.com',
+  projectId: 'chatapp-luxx',
+  storageBucket: 'chatapp-luxx.appspot.com',
+  messagingSenderId: '896382598167',
+  appId: '1:896382598167:web:b6907f0b04421a0224f052',
+};
