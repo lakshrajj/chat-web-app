16ed50812bc18f014e0c220ca37e4ae22c7b101f
Luxx <lakshrajme@gmail.com>
2022-06-25 19:17:28 +0530

    added realtime user State etc

---
+++ src/context/profile.context.js
@@ -0,0 +1,55 @@
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import { auth, database } from '../misc/firebase';
+
+const ProfileContext = createContext();
+
+export const ProfileProvider = ({ children }) => {
+  const [profile, setProfile] = useState(null);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    let userRef;
+
+    const authUnsub = auth.onAuthStateChanged(authObj => {
+      if (authObj) {
+        userRef = database.ref(`/profiles/${authObj.uid}`);
+
+        userRef.on('value', snap => {
+          const { name, createdAt } = snap.val();
+
+          const data = {
+            name,
+            createdAt,
+            uid: authObj.uid,
+            email: authObj.email,
+          };
+
+          setProfile(data);
+          setLoading(false);
+        });
+      } else {
+        if (userRef) {
+          userRef.off();
+        }
+        setProfile(null);
+        setLoading(false);
+      }
+    });
+
+    return () => {
+      authUnsub();
+      if (userRef) {
+        userRef.off();
+      }
+    };
+  }, []);
+
+  return (
+    // eslint-disable-next-line react/jsx-no-constructed-context-values
+    <ProfileContext.Provider value={{ isLoading, profile }}>
+      {children}
+    </ProfileContext.Provider>
+  );
+};
+
+export const useProfile = () => useContext(ProfileContext);
