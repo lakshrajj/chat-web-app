ed60ebe378d08a28a403428aba85002151c33321
Luxx <lakshrajme@gmail.com>
2022-06-27 23:48:03 +0530

    Building Dashboard #1 ---Signout Button and User Name on DashBrd

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import { Button, Drawer } from 'rsuite';
+import { useProfile } from '../../context/profile.context';
 
-const Dashboard = () => {
-  return <div>index</div>;
+const Dashboard = ({ onSignOut }) => {
+  const { profile } = useProfile();
+
+  return (
+    <>
+      <Drawer.Header>
+        <Drawer.Title>DashBoard</Drawer.Title>
+      </Drawer.Header>
+
+      <Drawer.Body>
+        <h3>Hey , {profile.name}</h3>
+      </Drawer.Body>
+
+      <Drawer.Footer>
+        <Button onClick={onSignOut} block color="red">
+          Sign Out
+        </Button>
+      </Drawer.Footer>
+    </>
+  );
 };
 
 export default Dashboard;
