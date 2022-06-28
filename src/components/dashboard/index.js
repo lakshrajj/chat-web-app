01b88cd7e6e71facd64c585975f26d83d32b1999
Luxx <lakshrajme@gmail.com>
2022-06-28 11:45:20 +0530

    Creating Dashboard #3

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import { Button, Divider, Drawer } from 'rsuite';
+import { Alert, Button, Divider, Drawer } from 'rsuite';
 import { useProfile } from '../../context/profile.context';
+import { database } from '../../misc/firebase';
 import Editableinput from '../Editableinput';
 
 const Dashboard = ({ onSignOut }) => {
   const { profile } = useProfile();
 
-  const onSave = newData => {
-    console.log(newData);
+  const onSave = async newData => {
+    const userNickNameRef = database
+      .ref(`/profiles/${profile.uid}`)
+      .child('name');
+
+    try {
+      await userNickNameRef.set(newData);
+      Alert.success('Nickname has been Updated.', 4000);
+    } catch (err) {
+      Alert.error(err.message, 4000);
+    }
   };
 
   return (
