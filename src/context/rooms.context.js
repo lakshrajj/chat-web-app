120883281334d3882224e1d867a38aeaaff8052b
Luxx <lakshrajme@gmail.com>
2022-07-02 00:27:19 +0530

    Creating Chat Room #2

---
+++ src/context/rooms.context.js
@@ -0,0 +1,24 @@
+import React, { createContext, useEffect, useState } from 'react';
+import { database } from '../misc/firebase';
+import { transformArrWithId } from '../misc/helpers';
+
+const RoomsContext = createContext();
+
+export const RoomsProvider = ({ children }) => {
+  const [rooms, setRooms] = useState(null);
+
+  useEffect(() => {
+    const roomListRef = database.ref('rooms');
+    roomListRef.on('value', snap => {
+      const data = transformArrWithId(snap.val());
+      setRooms(data);
+    });
+
+    return () => {
+      roomListRef.off();
+    };
+  }, []);
+  return (
+    <RoomsContext.Provider value={rooms}>{children}</RoomsContext.Provider>
+  );
+};
