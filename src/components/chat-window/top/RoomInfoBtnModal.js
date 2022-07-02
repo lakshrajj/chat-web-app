35656c3ed75f0fb66b4e75731f2e3fe68d80e1bd
Luxx <lakshrajme@gmail.com>
2022-07-02 10:36:24 +0530

    Chat Room Top :  Created modal for chat room

---
+++ src/components/chat-window/top/RoomInfoBtnModal.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Button, Modal } from 'rsuite';
+import { useCurrentRoom } from '../../../context/current-room.context';
+import { useModalState } from '../../../misc/custom-hooks';
+
+const RoomInfoBtnModal = () => {
+  const { isOpen, open, close } = useModalState();
+  const des = useCurrentRoom(v => v.des);
+  const name = useCurrentRoom(v => v.name);
+
+  return (
+    <>
+      <Button onClick={open} appearance="link" className="px-0">
+        Room Information
+      </Button>
+
+      <Modal show={isOpen} onHide={close}>
+        <Modal.Header>
+          <Modal.Title>About {name}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h6 className="mb-1">Description</h6>
+          <p>{des}</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button block onClick={close}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+
+export default RoomInfoBtnModal;
