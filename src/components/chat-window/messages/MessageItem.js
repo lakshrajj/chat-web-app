8282f08b4b35043a6caded3aea2efbb19d9cae4a
Luxx <lakshrajme@gmail.com>
2022-07-03 12:33:31 +0530

    clickable profile inside Chatting room

--- src/components/chat-window/messages/MessageItem.js
+++ src/components/chat-window/messages/MessageItem.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import TimeAgo from 'timeago-react';
 import ProfileAvatar from '../../dashboard/ProfileAvatar';
+import ProfileInfoBtnModal from './ProfileInfoBtnModal';
 
 const MessageItem = ({ messages }) => {
   const { author, createdAt, text } = messages;
@@ -13,7 +14,12 @@ const MessageItem = ({ messages }) => {
           className="ml-1"
           size="xs"
         />
-        <spna className="ml-2">{author.name}</spna>
+
+        <ProfileInfoBtnModal
+          profile={author}
+          appearance="link"
+          className="p-0 ml-1 text-black"
+        />
         <TimeAgo
           datetime={createdAt}
           className="font-normal text-black-45 ml-2"
